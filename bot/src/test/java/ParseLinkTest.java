e3f83e97fec96650aa35a878e5c2d2f1cfaa9329
Ilnur37 <dinmukhameto@mail.ru>
2024-02-12 17:00:00 +0300

    hw1 | Update bot.yml

---
+++ bot/src/test/java/ParseLinkTest.java
@@ -0,0 +1,31 @@
+import com.pengrad.telegrambot.model.Update;
+import edu.java.bot.BotApplication;
+import edu.java.bot.entity.Domain;
+import edu.java.bot.entity.Link;
+import edu.java.bot.repository.UserRepository;
+import edu.java.bot.service.handler.StartCommand;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest(classes = {BotApplication.class})
+public class ParseLinkTest {
+    @MockBean
+    Update update;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    StartCommand start;
+
+    @Test
+    @DisplayName("Преобразование ссылки в Link")
+    void parseLink() {
+        String link = "https://github.com/Ilnur37/Content-Checker";
+        Link actualLink = start.parsePartOfMessageIntoLink(link);
+        Link expectedLink = new Link(new Domain("https://github.com"), "/Ilnur37/Content-Checker");
+        Assertions.assertEquals(expectedLink, actualLink);
+    }
+}
