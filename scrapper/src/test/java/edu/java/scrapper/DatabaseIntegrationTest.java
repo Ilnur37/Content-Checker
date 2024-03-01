66b5f0a7d0c71748f5a26df0423586057af36b0f
Ilnur37 <dinmukhameto@mail.ru>
2024-03-01 17:44:03 +0300

    Hw 4

--- scrapper/src/test/java/edu/java/scrapper/DatabaseIntegrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/DatabaseIntegrationTest.java
@@ -5,7 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class DatabaseIntegrationTest extends IntegrationTest {
-
     @Test
     void testContainerStartup() {
         //пустой тест для проверки запуска контейнера
